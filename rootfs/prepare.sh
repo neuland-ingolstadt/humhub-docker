e64e7de9e38cfb6e8c5a5027718d52781a528e81
Alexander Horn <alexhorn@users.noreply.github.com>
2023-01-08 17:38:53 +0100

    Enable URL rewriting

--- rootfs/prepare.sh
+++ rootfs/prepare.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 apt update
 apt install -y -o DPkg::Options::="--force-confold" supervisor cron build-essential libmagickwand-dev libzip-dev libldap2-dev
 
+a2enmod rewrite
+
 pecl install imagick apcu
 docker-php-ext-enable imagick apcu
 docker-php-ext-configure gd --with-jpeg
