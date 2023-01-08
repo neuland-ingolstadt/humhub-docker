e99e163c6a98188db9b56f646c1f8def203dbbba
Alexander Horn <alexhorn@users.noreply.github.com>
2023-01-08 19:26:50 +0100

    Clean up after build

--- rootfs/download.sh
+++ rootfs/download.sh
@@ -2,10 +2,9 @@
 set -euxo pipefail
 
 cd /tmp
-curl -O https://download.humhub.com/downloads/install/humhub-$HUMHUB_VERSION.tar.gz
-tar xvfz humhub-$HUMHUB_VERSION.tar.gz
+curl https://download.humhub.com/downloads/install/humhub-$HUMHUB_VERSION.tar.gz | tar xvz
 rm -rf /var/www/html && mv humhub-$HUMHUB_VERSION /var/www/html
-cp /var/www/html/.htaccess.dist /var/www/html/.htaccess
+mv /var/www/html/.htaccess.dist /var/www/html/.htaccess
 chown -R www-data:www-data /var/www/html
 
 sed -i '/YII_DEBUG/s/^\/*/\/\//' /var/www/html/index.php
--- rootfs/prepare.sh
+++ rootfs/prepare.sh
@@ -2,11 +2,13 @@
 set -euxo pipefail
 
 apt update
-apt install -y -o DPkg::Options::="--force-confold" supervisor busybox-static build-essential libmagickwand-dev libzip-dev libldap2-dev
-
-a2enmod rewrite
+apt install -y -o DPkg::Options::="--force-confold" supervisor busybox-static libmagickwand-dev libzip-dev libldap2-dev
 
 pecl install imagick apcu
 docker-php-ext-enable imagick apcu
 docker-php-ext-configure gd --with-jpeg
 docker-php-ext-install gd intl exif zip ldap pdo pdo_mysql
+
+rm -rf /var/lib/apt/lists/*
+
+a2enmod rewrite
